be48a378b7225d829c647bbe36768eb713084179
ErikNgigi <ericmosesngigi@gmail.com>
2023-05-03 12:04:27 +0300

    feat(chromium homepage): background color and openweather api add

---
+++ bookmarks.js
@@ -0,0 +1,33 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "EricNgigi", url: "https://ericngigi.netlify.app/" },
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com/ErikNgigi" },
+      { name: "Linkedin", url: "https://www.linkedin.com/in/eric-ngigi-073b72153/" },
+      { name: "ChatGPT", url: "https://chat.openai.com/auth/login" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Pahe", url: "https://pahe.li" },
+      { name: "RARBG", url: "https://rarbgget.org/torrents.php"},
+      { name: "Kitsu", url: "https://kitsu.io/explore/anime" },
+      { name: "Nyaa", url: "https://nyaa.si/" },
+    ],
+  },
+
+  {
+    title: "Arch Linux"
+    links: [
+      { name: "ArchWiki", url: "https://wiki.archlinux.org/" },
+      { name: "Packages", url: "https://archlinux.org/packages/" },
+      { name: "AUR", url: "https://aur.archlinux.org/" },
+      { name: "News", url: "https://archlinux.org/" },
+    ]
+  }
+];
