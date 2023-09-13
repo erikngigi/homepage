bc997036ad88f7faa727e01b9cdcb2095a78a85e
ErikNgigi <ericmosesngigi@gmail.com>
2023-09-13 16:17:37 +0300

    reforked repository

---
+++ bookmarks.js
@@ -0,0 +1,56 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Eric Ngigi", url: "https://ericngigi.com" },
+      { name: "ChatGPT", url: "https://chat.openai.com/" },
+      { name: "TOP", url: "https://www.theodinproject.com/dashboard" },
+      { name: "HackerRank", url: "https://www.hackerrank.com/" },
+    ],
+  },
+  {
+    title: "Work",
+    links: [
+      { name: "GitHub", url: "https://github.com/ErikNgigi" },
+      { name: "Linkedin", url: "https://www.linkedin.com/in/eric-ngigi-073b72153/" },
+      { name: "ALX", url: "https://member.theroom.com/alx/" },
+      { name: "W3Schools", url: "https://my-learning.w3schools.com/" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "YouTube", url: "https://youtube.com" },
+      { name: "Pahe", url: "https://pahe.li" },
+      { name: "Nyaa", url: "https://nyaa.si/" },
+      { name: "GogoAnime", url: "https://gogoanime.cl/" },
+    ],
+  },
+  {
+    title: "Arch",
+    links: [
+      { name: "ArchWiki", url: "https://wiki.archlinux.org/" },
+      { name: "Packages", url: "https://archlinux.org/packages/" },
+      { name: "AUR", url: "https://aur.archlinux.org/" },
+      { name: "News", url: "https://archlinux.org/" },
+    ],
+  },
+  {
+    title: "GMail",
+    links: [
+      { name: "ericwanyoike", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike@gmail.com" },
+      { name: "ericmosesngigi", url: "https://mail.google.com/mail/u/?authuser=ericmosesngigi@gmail.com" },
+      { name: "ericwanyoike772", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike772@gmail.com" },
+      { name: "ericthegiggs", url: "https://mail.google.com/mail/u/?authuser=ericthegiggs@gmail.com" },
+    ],
+  },
+  {
+    title: "Local",
+    links: [
+      { name: "hugo", url: "http://localhost:1313" },
+      { name: "algoendeavours", url: "http://localhost:8080/algoendeavours/" },
+      { name: "php-my-admin", url: "http://localhost:8080/phpmyadmin/" },
+    ],
+  },
+];
